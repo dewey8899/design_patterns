0b2de5d697914da1a44f918c876712b7190ab0de
杜威 <duwei@vender.risingauto.com>
2023-09-25 23:03:04 +0800

    设计模式 之 备忘录模式 可以恢复到上一个状态

---
+++ src/test/java/com/dewey/design_patterns/responsbilitychain/AbstractLoggerTest.java
@@ -0,0 +1,28 @@
+package com.dewey.design_patterns.responsbilitychain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AbstractLoggerTest {
+
+    private AbstractLogger abstractLoggerUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        abstractLoggerUnderTest = new AbstractLogger() {
+            @Override
+            protected void write(String message) {
+
+            }
+        };
+    }
+
+    @Test
+    void testLogMessage() {
+        // Setup
+        // Run the test
+        abstractLoggerUnderTest.logMessage(0, "message");
+
+        // Verify the results
+    }
+}
